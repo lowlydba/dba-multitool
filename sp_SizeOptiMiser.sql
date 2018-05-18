d33bec1d949ece7af0dc32ee4ba821b9b9c91260
John McCall <LowlyDBA@users.noreply.github.com>
2018-05-18 18:18:39 -0400

    building half of the nvarchar check - lite version only

--- sp_SizeOptiMiser.sql
+++ sp_SizeOptiMiser.sql
@@ -21,17 +21,18 @@ CREATE TABLE #results (
 
 DECLARE @isExpress BIT = 0;
 DECLARE @getGreedy BIT = 0;
+DECLARE @lastUpdated NVARCHAR(100) = '2018-05-18'
 
 /* Find edition */
 IF (CAST(SERVERPROPERTY('Edition') AS VARCHAR(50))) LIKE '%express%'
 	SET @isExpress = 1;
 
 INSERT INTO #results
-SELECT '0', 'Let''s do this', 'Vroom, vroom', 'Off to the races!', 'Ready, set, go!', 'Last Updated 12/20/2017', 'http://expressdb.io';
+SELECT '0', 'Let''s do this', 'Vroom, vroom', 'Off to the races!', 'Ready, set, go!', 'Last Updated ' + @lastUpdated, 'http://expressdb.io';
 
 /* Check 1: Did you mean to use a time based format? */
 INSERT INTO #results
-SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), c.name, N'Column storing date should use a date or datetime format, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
+SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Column storing date should use a date or datetime format, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
 FROM sys.columns as c
 	inner join sys.tables as t on t.object_id = c.object_id
 	inner join sys.types as ty on ty.user_type_id = c.user_type_id
@@ -41,7 +42,7 @@ WHERE c.is_identity = 0 --exclude identity cols
 	AND ty.name NOT IN ('datetime', 'datetime2', 'datetimeoffset', 'date', 'smalldatetime')
 	
 INSERT INTO #results 
-SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), c.name, N'Column storing time should use a time, datetime, or sometimes integer format, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
+SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Column storing time should use a time, datetime, or sometimes integer format, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
 FROM sys.columns as c
 	inner join sys.tables as t on t.object_id = c.object_id
 	inner join sys.types as ty on ty.user_type_id = c.user_type_id
@@ -52,7 +53,7 @@ WHERE c.is_identity = 0 --exclude identity cols
 	
 /* Check 2: Old School Variable Lengths (255/256) */
 INSERT INTO #results 
-SELECT 2, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), c.name, N'Possible arbitrary variable length column in use. Is the ' + ty.name + ' length of ' + CAST (c.max_length / 2 AS varchar(10)) + ' based on real requirements?', N'https://goo.gl/uiltVb'
+SELECT 2, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Possible arbitrary variable length column in use. Is the ' + ty.name + ' length of ' + CAST (c.max_length / 2 AS varchar(10)) + ' based on real requirements?', N'https://goo.gl/uiltVb'
 FROM sys.columns as c
 	inner join sys.tables as t on t.object_id = c.object_id
 	inner join sys.types as ty on ty.user_type_id = c.user_type_id
@@ -61,7 +62,7 @@ WHERE c.is_identity = 0 --exclude identity cols
 	AND ty.name = 'nvarchar'
 	AND c.max_length IN (510, 512)
 UNION
-SELECT 2, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), c.name, N'Possible arbitrary variable length column in use. Is the ' + ty.name + ' length of ' + CAST (c.max_length AS varchar(10)) + ' based on real requirements?', N'https://goo.gl/uiltVb'
+SELECT 2, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Possible arbitrary variable length column in use. Is the ' + ty.name + ' length of ' + CAST (c.max_length AS varchar(10)) + ' based on real requirements?', N'https://goo.gl/uiltVb'
 FROM sys.columns as c
 	inner join sys.tables as t on t.object_id = c.object_id
 	inner join sys.types as ty on ty.user_type_id = c.user_type_id
@@ -71,41 +72,62 @@ WHERE c.is_identity = 0 --exclude identity cols
 	AND c.max_length IN (255, 256)
 	
 /* Check 3: Mad MAX - Varchar(MAX) */
+INSERT INTO #results
+SELECT 3, N'Mad NVARCHAR(MAX)', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), 
+		N'Column is NVARCHAR(MAX) which allows very large row sizes. Should there be a character limit?', N'https://goo.gl/uiltVb'
+FROM sys.columns as c
+	inner join sys.tables as t on t.object_id = c.object_id
+	inner join sys.types as ty on ty.user_type_id = c.user_type_id
+WHERE t.is_ms_shipped = 0 --exclude sys table
+	AND ty.[name] = 'nvarchar'
+	AND c.max_length = -1
 
-
-/* Check 4: User DB or model db  Growth set past 10GB */
-INSERT INTO #results 
-select 4, N'Database Growth', 'DATABASE', QUOTENAME(DB_NAME(database_id)), NULL, N'Database file ' + name + ' has a maximum growth set to ' + CASE 
-		WHEN max_size = -1 
-			THEN 'Unlimited'
-		WHEN max_size > 0
-			THEN CAST((max_size / 1024) * 8 AS VARCHAR(MAX))
-	END + ', which is over the user database maximum file size of 10GB.', 'http://'
-	from sys.master_files mf
-where (max_size > 1280000 OR max_size = -1) -- greater than 10GB or unlimited
-	AND MF.database_id > 4
-	AND data_space_id > 0 -- limit doesn't apply to log files
+/* Check 4: User DB or model db  Growth set past 10GB - ONLY IF EXPRESS*/
+if (@isExpress = 1)
+	BEGIN
+		INSERT INTO #results 
+		select 4, N'Database Growth', 'DATABASE', QUOTENAME(DB_NAME(database_id)), NULL, N'Database file ' + name + ' has a maximum growth set to ' + CASE 
+				WHEN max_size = -1 
+					THEN 'Unlimited'
+				WHEN max_size > 0
+					THEN CAST((max_size / 1024) * 8 AS VARCHAR(MAX))
+			END + ', which is over the user database maximum file size of 10GB.', 'http://'
+			from sys.master_files mf
+		where (max_size > 1280000 OR max_size = -1) -- greater than 10GB or unlimited
+			AND MF.database_id > 4
+			AND data_space_id > 0 -- limit doesn't apply to log files
+	END
 
 /* Check 5: User DB or model db growth set to % */
 INSERT INTO #results
 select 5, N'Database Growth', 'DATABASE', QUOTENAME(DB_NAME(database_id)), NULL, N'Database file ' + name + 
-	' has growth set to % instead of a fixed amount. This is likely to grow too fast and hit a logical or feature size limit of 10GB.', 'http://'
+	' has growth set to % instead of a fixed amount. This is likely to grow too fast.', 'http://'
 	from sys.master_files mf
 where MF.database_id > 4 --Not a system DB
 	AND is_percent_growth = 1 
 	AND data_space_id = 1 --ignore log files
 
 
-/* Check 6: Do you really need Nvarchar - possible scan of data? */
+/* Check 6: Do you really need Nvarchar - possible scan of data?*/
 IF (@getGreedy = 1 AND @isExpress = 1 )
 	BEGIN
-	 PRINT 'Checking for nvarchar existence...'
+		PRINT 'Checking for nvarchar existence...'
+	END
+	ELSE IF (@isExpress = 1) --Too many possible columns to return in a non-express instance
+		BEGIN
+		SELECT 3, N'Data Types', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), 
+			N'Column is NVARCHAR. If Unicode characters aren''t required, try VARCHAR which is 1 byte/character instead of 2 byte/character.', N'https://goo.gl/uiltVb'
+		FROM sys.columns as c
+			inner join sys.tables as t on t.object_id = c.object_id
+			inner join sys.types as ty on ty.user_type_id = c.user_type_id
+		WHERE t.is_ms_shipped = 0 --exclude sys table
+			AND ty.[name] = 'nvarchar'
 	END
 
 /* Check 7: BIGINT for identity values - sure its needed ?  - ONLY IF EXPRESS*/
 IF (@isExpress = 1)
 	BEGIN
-		SELECT 7, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), c.name, N'BIGINT used on IDENTITY column in SQL Express. If values will never exceed 2,147,483,647 use INT instead.', N'https://goo.gl/uiltVb'
+		SELECT 7, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'BIGINT used on IDENTITY column in SQL Express. If values will never exceed 2,147,483,647 use INT instead.', N'https://goo.gl/uiltVb'
 		FROM sys.columns as c
 			inner join sys.tables as t on t.object_id = c.object_id
 			inner join sys.types as ty on ty.user_type_id = c.user_type_id
@@ -116,7 +138,7 @@ IF (@isExpress = 1)
 
 /* Check 8: Don't use FLOAT or REAL */
 INSERT INTO #results
-select 8, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), ac.name, N'Are you sure you want to use ' + st.name + ' and not DECIMAL/NUMERIC?', N'https://goo.gl/uiltVb'
+select 8, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), QUOTENAME(ac.name), N'Are you sure you want to use ' + st.name + ' and not DECIMAL/NUMERIC?', N'https://goo.gl/uiltVb'
 from sys.all_columns as ac
     inner join sys.objects as o on o.object_id = ac.object_id
     inner join sys.systypes as st on st.xtype = ac.system_type_id
@@ -125,14 +147,14 @@ where st.name IN ('float', 'real')
 
 /* Check 9: Don't use deprecated values (NTEXT, TEXT, IMAGE) */
 INSERT INTO #results
-select 9, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), ac.name, N'Deprecated data type in use: ' + st.name + '.', N'https://goo.gl/u9SgEj'
+select 9, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), QUOTENAME(ac.name), N'Deprecated data type in use: ' + st.name + '.', N'https://goo.gl/u9SgEj'
 from sys.all_columns as ac
     inner join sys.objects as o on o.object_id = ac.object_id
     inner join sys.systypes as st on st.xtype = ac.system_type_id
 where st.name IN ('next', 'text', 'image')
     and o.type_desc = 'USER_TABLE'
 
-/* Check 10: Non-default fill factor */
+/* Check 10: Non-default fill factor - ONLY IF EXPRESS*/
 /* Check 11: Questionable number of indexes */
 INSERT INTO #results
 SELECT 11, 'Lotsa Indexes','INDEXES', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name) ,NULL, 'There are ' + CAST(COUNT(DISTINCT(i.index_id)) AS VARCHAR) + ' indexes on this table taking up ' + CAST(CAST(SUM(s.[used_page_count]) * 8 / 1024.00 AS DECIMAL(10,2)) AS VARCHAR) + ' MB of space.', 'http'
@@ -151,15 +173,13 @@ HAVING COUNT(DISTINCT(i.index_id)) > 7;
 FROM sys.tables t
 WHERE t.is_ms_shipped = 0*/
 
-/* CHeck 14: numeric or decimal without trailing 0s */
+/* Check 14: numeric or decimal without trailing 0s */
 INSERT INTO #results
-SELECT 14, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), ac.name, N'Column is ' + UPPER(st.name) + '(' + CAST(ac.precision AS VARCHAR) + ',' + CAST(ac.scale AS VARCHAR) + ')' + '. Consider using an INT variety for space reduction.', N'https://goo.gl/agh5CA'
+SELECT 14, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), QUOTENAME(ac.name), N'Column is ' + UPPER(st.name) + '(' + CAST(ac.precision AS VARCHAR) + ',' + CAST(ac.scale AS VARCHAR) + ')' + '. Consider using an INT variety for space reduction.', N'https://goo.gl/agh5CA'
 FROM sys.objects as o
     inner join sys.all_columns as ac ON ac.object_id = o.object_id
     INNER JOIN sys.systypes as st on st.xtype = ac.system_type_id
 WHERE ac.scale = 0
     AND st.name IN ('decimal', 'numeric')
-
-	
 
 select * from #results;
