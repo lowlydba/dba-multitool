d2fba1e42e010106b419da90ea34a0893486adf1
John McCall <LowlyDBA@users.noreply.github.com>
2018-05-22 22:07:11 -0400

    Print initial gathered info

--- sp_SizeOptiMiser.sql
+++ sp_SizeOptiMiser.sql
@@ -1,14 +1,40 @@
 /* TO DO: 
-* Get more specific about ideal time formats (#1) and make sure date and time dont create dups for same columns 
+* 
 */
 
 SET NOCOUNT ON;
+
+PRINT 'sp_OptiMizer'
+PRINT '------------'
+PRINT ''
 
 --@GetGreedy 
 IF OBJECT_ID(N'tempdb..#results') IS NOT NULL
 DROP TABLE #results
 GO
 
+DECLARE @isExpress BIT = 0;
+DECLARE @getGreedy BIT = 0;
+DECLARE @lastUpdated NVARCHAR(20) = '2018-05-18'
+DECLARE @version SMALLINT = 0;
+DECLARE @fullVersion NVARCHAR(50) = (SELECT @@VERSION)
+
+/* Find edition */
+IF (CAST(SERVERPROPERTY('Edition') AS VARCHAR(50))) LIKE '%express%'
+	SET @isExpress = 1;
+	
+/* Find Version */
+SET @version = (SELECT CAST(LEFT(CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR), CHARINDEX('.', CAST(SERVERPROPERTY('ProductVersion') AS NVARCHAR), 0) - 1) AS INT))
+
+/* Print info */
+
+PRINT 'Time: 				' + CAST(GETDATE() AS NVARCHAR(50))
+PRINT 'Express Edition: 	' + CAST(@isExpress AS CHAR(1))
+PRINT 'SQL Major Version: 	' + CAST(@version AS VARCHAR(2))
+PRINT '@getGreedy: 		' + CAST(@getGreedy AS CHAR(1))
+PRINT ''
+
+PRINT 'Building results table...'
 CREATE TABLE #results (
 [ID] INT IDENTITY(1,1) NOT NULL PRIMARY KEY,
 [check_num] INT NOT NULL,
@@ -19,38 +45,23 @@ CREATE TABLE #results (
 [message] NVARCHAR(250) NULL,
 [ref_link] NVARCHAR(500) NULL);
 
-DECLARE @isExpress BIT = 0;
-DECLARE @getGreedy BIT = 0;
-DECLARE @lastUpdated NVARCHAR(100) = '2018-05-18'
-
-/* Find edition */
-IF (CAST(SERVERPROPERTY('Edition') AS VARCHAR(50))) LIKE '%express%'
-	SET @isExpress = 1;
-
+/* Header row */
 INSERT INTO #results
 SELECT '0', 'Let''s do this', 'Vroom, vroom', 'Off to the races!', 'Ready, set, go!', 'Last Updated ' + @lastUpdated, 'http://expressdb.io';
 
+PRINT 'Running size checks...'
+
 /* Check 1: Did you mean to use a time based format? */
 INSERT INTO #results
-SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Column storing date should use a date or datetime format, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
+SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Columns storing date or time should use a temporal specific data type, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
 FROM sys.columns as c
 	inner join sys.tables as t on t.object_id = c.object_id
 	inner join sys.types as ty on ty.user_type_id = c.user_type_id
 WHERE c.is_identity = 0 --exclude identity cols
 	AND t.is_ms_shipped = 0 --exclude sys table
-	AND c.name LIKE '%date%' 
-	AND ty.name NOT IN ('datetime', 'datetime2', 'datetimeoffset', 'date', 'smalldatetime')
-	
-INSERT INTO #results 
-SELECT 1, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Column storing time should use a time, datetime, or sometimes integer format, but this column is using ' + ty.name + '.', N'https://goo.gl/uiltVb'
-FROM sys.columns as c
-	inner join sys.tables as t on t.object_id = c.object_id
-	inner join sys.types as ty on ty.user_type_id = c.user_type_id
-WHERE c.is_identity = 0 --exclude identity cols
-	AND t.is_ms_shipped = 0 --exclude sys table
-	AND c.name LIKE '%time%'
-	AND ty.name NOT IN ('datetime', 'datetime2', 'datetimeoffset', 'date', 'time', 'int')
-	
+	AND (c.name LIKE '%date%' OR c.name LIKE '%time%')
+	AND ty.name NOT IN ('datetime', 'datetime2', 'datetimeoffset', 'date', 'smalldatetime', 'time')
+
 /* Check 2: Old School Variable Lengths (255/256) */
 INSERT INTO #results 
 SELECT 2, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'Possible arbitrary variable length column in use. Is the ' + ty.name + ' length of ' + CAST (c.max_length / 2 AS varchar(10)) + ' based on real requirements?', N'https://goo.gl/uiltVb'
@@ -109,6 +120,7 @@ where MF.database_id > 4 --Not a system DB
 
 
 /* Check 6: Do you really need Nvarchar - possible scan of data?*/
+BEGIN
 IF (@getGreedy = 1 AND @isExpress = 1 )
 	BEGIN
 		PRINT 'Checking for nvarchar existence...'
@@ -123,8 +135,10 @@ IF (@getGreedy = 1 AND @isExpress = 1 )
 		WHERE t.is_ms_shipped = 0 --exclude sys table
 			AND ty.[name] = 'nvarchar'
 	END
+END
 
 /* Check 7: BIGINT for identity values - sure its needed ?  - ONLY IF EXPRESS*/
+BEGIN
 IF (@isExpress = 1)
 	BEGIN
 		SELECT 7, N'Data Formats', 'USER_TABLE', QUOTENAME(SCHEMA_NAME(t.schema_id)) + '.' + QUOTENAME(t.name), QUOTENAME(c.name), N'BIGINT used on IDENTITY column in SQL Express. If values will never exceed 2,147,483,647 use INT instead.', N'https://goo.gl/uiltVb'
@@ -135,8 +149,10 @@ IF (@isExpress = 1)
 			AND ty.name = 'BIGINT'
 			AND c.is_identity = 1 
 	END
+END
 
 /* Check 8: Don't use FLOAT or REAL */
+BEGIN
 INSERT INTO #results
 select 8, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), QUOTENAME(ac.name), N'Are you sure you want to use ' + st.name + ' and not DECIMAL/NUMERIC?', N'https://goo.gl/uiltVb'
 from sys.all_columns as ac
@@ -144,8 +160,10 @@ from sys.all_columns as ac
     inner join sys.systypes as st on st.xtype = ac.system_type_id
 where st.name IN ('float', 'real')
     and o.type_desc = 'USER_TABLE'
+END
 
 /* Check 9: Don't use deprecated values (NTEXT, TEXT, IMAGE) */
+BEGIN
 INSERT INTO #results
 select 9, 'Data Formats', o.type_desc, QUOTENAME(SCHEMA_NAME(o.schema_id)) + '.' + QUOTENAME(o.name), QUOTENAME(ac.name), N'Deprecated data type in use: ' + st.name + '.', N'https://goo.gl/u9SgEj'
 from sys.all_columns as ac
@@ -153,6 +171,7 @@ from sys.all_columns as ac
     inner join sys.systypes as st on st.xtype = ac.system_type_id
 where st.name IN ('next', 'text', 'image')
     and o.type_desc = 'USER_TABLE'
+END
 
 /* Check 10: Non-default fill factor - ONLY IF EXPRESS*/
 /* Check 11: Questionable number of indexes */
@@ -168,10 +187,15 @@ GROUP BY t.name, t.schema_id
 HAVING COUNT(DISTINCT(i.index_id)) > 7;
 
 /* Check 12: Should sparse columns be used? */
-/* Check 13: Compression (2016 SP1+ only for express) */
+/* Check 13: Compression (2016 SP1+ only for express) 
+	What version for standard / enterprise? */
+BEGIN
+IF (@isExpress = 1 AND @fullVersion LIKE '13.0.4%')
+	SELECT 1;
 /*SELECT *
 FROM sys.tables t
 WHERE t.is_ms_shipped = 0*/
+END
 
 /* Check 14: numeric or decimal without trailing 0s */
 INSERT INTO #results
@@ -183,3 +207,5 @@ WHERE ac.scale = 0
     AND st.name IN ('decimal', 'numeric')
 
 select * from #results;
+
+PRINT 'Done!'
