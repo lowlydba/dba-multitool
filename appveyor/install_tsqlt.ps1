044c6f3c7254cbe09ca447e045f0a80b0dd28f94
John McCall <john@lowlydba.com>
2020-07-30 14:01:43 -0400

    Fix env vars in appveyor + typo

--- appveyor/install_tsqlt.ps1
+++ appveyor/install_tsqlt.ps1
@@ -1,8 +1,8 @@
 Write-Host "Installing tSQLt"
 
-$CLRScript = Join-Path $env:CLONE_FOLDER $env:TSQLTSETCLR
-$CreateDBScript = Join-Path $env:CLONE_FOLDER $env:TSQLTCREATEDB
-$tSQLtInstallScript = Join-Path $env:CLONE_FOLDER $env:TSQLTINSTALL
+$CLRScript = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:TSQLTSETCLR
+$CreateDBScript = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:TSQLTCREATEDB
+$tSQLtInstallScript = Join-Path $env:APPVEYOR_BUILD_FOLDER $env:TSQLTINSTALL
 
 Invoke-SqlCmd –ServerInstance $env:DB_INSTANCE -Database "master" -InputFile $clrscript -Username $env:MSSQL_LOGIN -Password $env:MSSQL_PASS -Verbose | Out-Null
 Invoke-SqlCmd –ServerInstance $env:DB_INSTANCE -Database "master" -InputFile $CreateDBScript -Username $env:MSSQL_LOGIN -Password $env:MSSQL_PASS -Verbose | Out-Null
