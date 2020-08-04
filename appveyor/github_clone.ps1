755201d034027aaca14d9b4e341db9f8342b1b6a
John McCall <John@lowlydba.com>
2020-08-04 18:13:09 -0400

    fix clone script - needs to be embedded

--- appveyor/github_clone.ps1
+++
@@ -1,6 +0,0 @@
-git config --global credential.helper store
-Add-Content "$HOME\.git-credentials" "https://$($env:access_token):x-oauth-basic@github.com`n" -NoNewLine
-git config --global user.email "appveyor@lowlydba.com"
-git config --global user.name "Appveyor"
-git config --global core.safecrlf false
-git clone -q --single-branch --branch=%APPVEYOR_PULL_REQUEST_HEAD_REPO_BRANCH% https://github.com/LowlyDBA/ExpressSQL.git %APPVEYOR_BUILD_FOLDER%
