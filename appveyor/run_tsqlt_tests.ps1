292bd4e6278ca8c2d3cdec4694af126e11d57fbc
John McCall <John@lowlydba.com>
2020-08-04 19:33:06 -0400

    setup Azure SQL build env

--- appveyor/run_tsqlt_tests.ps1
+++ appveyor/run_tsqlt_tests.ps1
@@ -1,13 +1,23 @@
 param( 
     [Parameter()] 
-    $FilePath = "tests\run",
-    $SqlInstance = $env:DB_INSTANCE,
-    $Database = $env:TARGET_DB,
-    $Color = "Green"
+    [string]$FilePath = "tests\run",
+    [string]$SqlInstance = $env:DB_INSTANCE,
+    [string]$Database = $env:TARGET_DB,
+    [string]$IsAzureSQL = $env:AzureSQL,
+    [string]$User = $env:AZURE_SQL_USER,
+    [string]$Pass = $env:AZURE_SQL_PASS,
+    [string]$Color = "Green"
     )
 
 Write-Host "Running tSQLt Tests..." -ForegroundColor $Color
 
-ForEach ($filename in Get-Childitem -Path $FilePath -Filter "*.sql") {
-    Invoke-SqlCmd -ServerInstance $SqlInstance -Database $Database -InputFile $filename.fullname -Verbose | Out-Null
+If ($IsAzureSQL -eq "True") {
+    ForEach ($filename in Get-Childitem -Path $FilePath -Filter "*.sql") {
+        Invoke-SqlCmd -ServerInstance $SqlInstance -Database $Database -InputFile $filename.fullname -Verbose -Username $User -Password $Pass | Out-Null
+    }
+}
+Else {
+    ForEach ($filename in Get-Childitem -Path $FilePath -Filter "*.sql") {
+        Invoke-SqlCmd -ServerInstance $SqlInstance -Database $Database -InputFile $filename.fullname -Verbose | Out-Null
+    }
 }
