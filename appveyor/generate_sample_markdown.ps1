b567322768f50fa1cd8cf5aac8364f5cf8209a3f
John McCall <John@lowlydba.com>
2020-09-14 20:15:04 -0400

    fix source database for sp_doc utility

--- appveyor/generate_sample_markdown.ps1
+++ appveyor/generate_sample_markdown.ps1
@@ -1,27 +1,30 @@
 param( 
     [Parameter()] 
     [string]$SqlInstance = $env:DB_INSTANCE,
+    [string]$UtilityDatabase = $env:TARGET_DB,
     [bool]$IsAzureSQL = [System.Convert]::ToBoolean($env:AzureSQL),
     [string]$User = $env:AZURE_SQL_USER,
     [string]$Pass = $env:AZURE_SQL_PASS,
     [string]$Color = "Green"
     )
 
-Write-Host "Generating WideWorldImporters markdown sample..." -ForegroundColor $Color
-
 $ErrorActionPreference = "Stop";
 $Url = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
 $BackupPath = "C:\WideWorldImporters-Full.bak"
-$Database = "WideWorldImporters"
-$master = "master"
+$SampleDatabase = "WideWorldImporters"
+
+Write-Host "Generating $SampleDatabase markdown sample..." -ForegroundColor $Color
+
+# Suppress DbaTools warning about SqlServer module also being loaded
+Set-DbatoolsConfig -Name Import.SqlpsCheck -Value $false -PassThru | Register-DbatoolsConfig
 
 # Download and restore WideWorldImporters sample database
-If (!(Get-DbaDatabase -SqlInstance $SqlInstance -Database $Database)) {
+If (!(Get-DbaDatabase -SqlInstance $SqlInstance -Database $SampleDatabase)) {
     Invoke-WebRequest -Uri $Url -OutFile $BackupPath
 
-    If (Test-Path $BackupPath) { Restore-DbaDatabase -SqlInstance $SqlInstance -DatabaseName $Database -Path $BackupPath -WithReplace }
-    Else { Write-Error "WideWorldImporters backup failed to download properly." }
+    If (Test-Path $BackupPath) { Restore-DbaDatabase -SqlInstance $SqlInstance -DatabaseName $SampleDatabase -Path $BackupPath -WithReplace | Out-Null }
+    Else { Write-Error "$SampleDatabase backup failed to download properly." }
 }
 
 # Generate documentation
-sqlcmd -S $SqlInstance -d $master -Q "EXEC sp_doc @DatabaseName = 'WideWorldImporters';" -o "docs/WideWorldImporters.md" -y 0
+sqlcmd -S $SqlInstance -d $UtilityDatabase -Q "EXEC sp_doc @DatabaseName = '$SampleDatabase';" -o "docs/$SampleDatabase.md" -y 0
