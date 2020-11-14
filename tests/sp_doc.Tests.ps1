0985ff6830a08442432892688728ba9e31bdfcdd
John McCall <John@lowlydba.com>
2020-11-14 15:19:22 -0500

    standardize on pester 5

--- tests/sp_doc.Tests.ps1
+++ tests/sp_doc.Tests.ps1
@@ -1,3 +1,4 @@
+#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.0.0" }
 
 BeforeAll {
     . "$PSScriptRoot\constants.ps1"
--- tests/sp_estindex.Tests.ps1
+++ tests/sp_estindex.Tests.ps1
@@ -1,47 +1,46 @@
 
+#Requires -Modules @{ ModuleName="Pester"; ModuleVersion="5.0.0" }
 
-Describe 'sp_estindex' {
-    Context 'tSQLt Tests' {    
+BeforeAll {
+    . "$PSScriptRoot\constants.ps1"
+}
+
+Describe "sp_estindex" {
+    Context "tSQLt Tests" {
         BeforeAll {
-            . "$PSScriptRoot\constants.ps1"
             $TestClass = "sp_estindex"
-            $Query = "EXEC tsqlt.Run '$TestClass'"
-            
+            $Query = "EXEC tSQLt.Run '$TestClass'"
+
             $Hash = @{
                 SqlInstance     = $SqlInstance
                 Database        = $Database
                 Query           = $Query
                 Verbose         = $true
                 EnableException = $true
-            }  
+            }
         }
-        It 'All tests' {
-
+        It "All tests" {
             If ($script:IsAzureSQL) {
-                
                 $SecPass = ConvertTo-SecureString -String $Pass -AsPlainText -Force
                 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecPass
                 $Hash.add("SqlCredential", $Credential)
 
-                { Invoke-DbaQuery @Hash } | Should -Not -Throw -Because "tsqlt unit tests must pass"
+                { Invoke-DbaQuery @Hash } | Should -Not -Throw -Because "tSQLt unit tests must pass"
             }
 
             Else {
-                { Invoke-DbaQuery @Hash } | Should -Not -Throw -Because "tsqlt unit tests must pass"
+                { Invoke-DbaQuery @Hash } | Should -Not -Throw -Because "tSQLt unit tests must pass"
             }
-        }     
+        }
     }
-    Context 'TSQLLint' {    
+    Context "TSQLLint" {
         BeforeAll {
-            . "$PSScriptRoot\constants.ps1"
             $Script = "sp_estindex.sql"
-            $TSQLLintConfig = ".\appveyor\tsqllint\.tsqllintrc_150"
 
             # TSQLLint results format: https://gist.github.com/LowlyDBA/caf744ce1a1498fee18e41d69d15f56d
             $LintResult = tsqllint -c $TSQLLintConfig $Script
             $LintErrors = $LintResult | Select-Object -Last 2 | Select-Object -First 1
             $LintWarnings = $LintResult | Select-Object -Last 2 | Select-Object -Last 1
-
         }
         It "Errors" {
             $LintErrors[0] | Should -Be "0" -Because "Lint errors are a no-no"
