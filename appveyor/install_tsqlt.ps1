56b5d2e4ec86f323e5cbce5332589ab504b5459b
John McCall <John@lowlydba.com>
2020-11-15 16:48:46 -0500

    remove copy of tsqlt - fetch automatically

--- appveyor/install_tsqlt.ps1
+++ appveyor/install_tsqlt.ps1
@@ -2,17 +2,31 @@ param(
     [Parameter()] 
     [String]$SqlInstance = $env:DB_INSTANCE,
     [String]$Database = $env:TARGET_DB,
-    [String]$CLRScript = "tests\tSQLt\SetClrEnabled.sql",
-    [String]$CreateDBScript = "tests\tSQLt\CreateDatabase.sql",
-    [String]$tSQLtInstallScript = "tests\tSQLt\tSQLt.class.sql",
     [String]$Color = "Green",
-    [String]$Master = "master",
     [string]$User = $env:AZURE_SQL_USER,
     [string]$Pass = $env:AZURE_SQL_PASS,
     [bool]$IsAzureSQL = [System.Convert]::ToBoolean($env:AzureSQL)
 )
 
-Write-Host "Installing tSQLt..." -ForegroundColor $Color
+Write-Host "Downloading and installing tSQLt..." -ForegroundColor $Color
+
+$DownloadUrl = "http://tsqlt.org/download/tsqlt/"
+$TempPath = [System.IO.Path]::GetTempPath()
+$ZipFile = Join-Path $TempPath "tSQLt.zip"
+$ZipFolder = Join-Path $TempPath "tSQLt"
+$SetupFile = Join-Path $ZipFolder "PrepareServer.sql"
+$InstallFile = Join-Path $ZipFolder "tSQLt.class.sql"
+$CreateDbQuery = "CREATE DATABASE [tsqlt];"
+
+# Download
+Try {
+    Invoke-TlsWebRequest $DownloadUrl -OutFile $ZipFile -ErrorAction Stop -UseBasicParsing
+    Expand-Archive -Path $ZipFile -DestinationPath $TempPath -Force
+}
+
+Catch {
+    Write-Error -Message "Error downloading tSQLt - try manually fetching from $DownloadUrl"
+}
 
 $Hash = @{
     SqlInstance     = $SqlInstance
@@ -20,16 +34,16 @@ $Hash = @{
     EnableException = $true
 }
 
+# Install
 If ($IsAzureSQL) {
     $SecPass = ConvertTo-SecureString -String $Pass -AsPlainText -Force
     $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $SecPass
     $Hash.add("SqlCredential", $Credential)
-
-    Invoke-DbaQuery @hash -File $tSQLtInstallScript
 }
 
 Else {
-    Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Master -File $clrscript | Out-Null
-    Invoke-DbaQuery -SqlInstance $SqlInstance -Database $Master -File $CreateDBScript | Out-Null
-    Invoke-DbaQuery @Hash -File $tSQLtInstallScript -MessagesToOutput
+    Invoke-DbaQuery -SqlInstance $SqlInstance -Database "master" -Query $CreateDbQuery
+    Invoke-DbaQuery @Hash -File $SetupFile
 }
+
+Invoke-DbaQuery @Hash -File $InstallFile
