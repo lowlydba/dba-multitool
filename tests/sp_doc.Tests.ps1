b60cfb83380aa3677c59226c748476c61a96663a
John McCall <John@lowlydba.com>
2020-12-02 20:05:24 -0500

    remove tsqllint from appveyor/pester

--- tests/sp_doc.Tests.ps1
+++ tests/sp_doc.Tests.ps1
@@ -38,24 +38,8 @@ Describe "sp_doc" {
                 Invoke-DbaQuery @Hash -File $File.FullName
             }
         }
-        It "All tests" { {
-                Invoke-DbaQuery @Hash -Query $RunTestQuery } | Should -Not -Throw -Because "tSQLt unit tests must pass"
-        }
-    }
-    Context "TSQLLint" {
-        BeforeAll {
-            $Script = "sp_doc.sql"
-
-            # TSQLLint results format: https://gist.github.com/LowlyDBA/caf744ce1a1498fee18e41d69d15f56d
-            $LintResult = tsqllint -c $TSQLLintConfig $Script
-            $LintErrors = $LintResult | Select-Object -Last 2 | Select-Object -First 1
-            $LintWarnings = $LintResult | Select-Object -Last 2 | Select-Object -Last 1
-        }
-        It "Errors" {
-            $LintErrors[0] | Should -Be "0" -Because "Lint errors are a no-no"
-        }
-        It "Warnings" {
-            $LintWarnings[0] | Should -Be "0" -Because "Lint warnings are a no-no"
+        It "All tests" {
+                { Invoke-DbaQuery @Hash -Query $RunTestQuery } | Should -Not -Throw -Because "tSQLt unit tests must pass"
         }
     }
 }
