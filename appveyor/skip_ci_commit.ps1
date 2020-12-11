0a5c2bd17271db95bb54b93f4fa25ff04f9227c7
John McCall <John@lowlydba.com>
2020-12-11 18:28:07 -0500

    Update skip_ci_commit.ps1

--- appveyor/skip_ci_commit.ps1
+++ appveyor/skip_ci_commit.ps1
@@ -1,5 +1,5 @@
 # Exit build with success if commit is just files generated from CI
 If ($env:APPVEYOR_REPO_COMMIT_MESSAGE -eq $env:CI_COMMIT_MSG) {
     Write-Output "Commit is CI produced files only. Skipping build."
-    Exit-Appveyor
+    Exit-AppveyorBuild
 }
