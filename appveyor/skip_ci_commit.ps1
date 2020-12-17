6280bb5865df0e9826524a0f42fdd1962e7871ac
John McCall <John@lowlydba.com>
2020-12-17 15:52:45 -0500

    remove skip ci commit, can disguise issues in non-2019 builds

--- appveyor/skip_ci_commit.ps1
+++
@@ -1,5 +0,0 @@
-# Exit build with success if commit is just files generated from CI
-If ($env:APPVEYOR_REPO_COMMIT_MESSAGE -eq $env:CI_COMMIT_MSG) {
-    Write-Output "Commit is CI produced files only. Skipping build."
-    Exit-AppveyorBuild
-}
