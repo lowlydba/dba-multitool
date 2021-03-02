0e8adcc6e1f100df8a33f037a07814842e9a47cb
John McCall <John@lowlydba.com>
2021-03-02 17:56:36 -0500

    bump version

--- sp_doc.sql
+++ sp_doc.sql
@@ -78,7 +78,7 @@ sp_doc - Always have current documentation by generating it on the fly in markdo
 
 Part of the DBA MultiTool http://dba-multitool.org
 
-Version: 20201221
+Version: 20210302
 
 MIT License
 
