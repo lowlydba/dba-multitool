9fd44caed68e3fec2e0393afa7480739c3b7ef01
John McCall <john.harold.mccall@gmail.com>
2021-06-29 15:40:17 -0400

    bump version

--- sp_doc.sql
+++ sp_doc.sql
@@ -85,7 +85,7 @@ sp_doc - Always have current documentation by generating it on the fly in markdo
 
 Part of the DBA MultiTool http://dba-multitool.org
 
-Version: 20210624
+Version: 20210629
 
 MIT License
 
