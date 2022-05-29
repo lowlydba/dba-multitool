840405be67c87f90927c7bc11360003cab62d0c3
John McCall <john@lowlydba.com>
2022-05-28 20:41:43 -0400

    debug codecov

--- .github/actions/sqlcover/main.ps1
+++ .github/actions/sqlcover/main.ps1
@@ -8,7 +8,9 @@ param(
 )
 
 $Package = "GOEddie.SQLCover"
-$endCheckSeconds = 5
+$sleepSeconds = 5
+$covStopFile = "cov_stop.txt"
+$covCompleteFile = "cov_complete.txt"
 
 if ($Env:RUNNER_OS -ne "Windows") {
     Write-Error "This action only supported on Windows runners." -ErrorAction "Stop"
@@ -39,14 +41,17 @@ if ($Action -eq "start") {
     $null = Start-Job -Name "SQLCover Trace" -ScriptBlock {
         $stop = $null
         while ($null -eq $stop) {
-            Start-Sleep -Seconds $endCheckSeconds
-            $stop = Get-ChildItem -Path $Env:RUNNER_TEMP -Filter "stop.txt"
+            Start-Sleep -Seconds $sleepSeconds
+            $stop = Get-ChildItem -Path $Env:RUNNER_TEMP -Filter $covStopFile
         }
         $coverageResults = $sqlCover.Stop()
 
         # Save results
         $coverageResults.Cobertura() | Out-File (Join-Path $OutputPath "coverage.xml") -Encoding utf8
         $coverageResults.SaveSourceFiles($OutputPath)
+
+        # Signal coverage completion
+        New-Item -Path $Env:RUNNER_TEMP -Name $covCompleteFile
     }
 }
 elseif ($Action -eq "stop") {
@@ -54,8 +59,16 @@ elseif ($Action -eq "stop") {
         Write-Output "Stopping SQLCover."
 
         # Create file to trigger tracing stop
-        New-Item -Path $Env:RUNNER_TEMP -Name "stop.txt"
-        Start-Sleep -Seconds ($endCheckSeconds + 10)
+        $null = New-Item -Path $Env:RUNNER_TEMP -Name $covStopFile
+
+        # Wait for coverage to dump
+        $coverageComplete = $null
+        while ($null -eq $stop) {
+            Start-Sleep -Seconds $sleepSeconds
+            $coverageComplete = Get-ChildItem -Path $Env:RUNNER_TEMP -Filter $covCompleteFile
+        }
+
+        Write-Output "Results saved."
     }
     catch {
         Write-Error "Error stopping SQLCover: $($_.Exception.Message)" -ErrorAction "Stop"
