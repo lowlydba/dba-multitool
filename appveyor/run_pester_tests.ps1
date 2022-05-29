bb0380f66b3d52425ff509328077b9e97ebbc5cf
John McCall <john@lowlydba.com>
2022-05-29 12:22:48 -0400

    fix appveyor?

--- appveyor/run_pester_tests.ps1
+++ appveyor/run_pester_tests.ps1
@@ -9,7 +9,7 @@ param(
     [switch]$LocalTest,
     [string]$CoverageXMLPath = $env:COV_REPORT,
     [string]$SqlInstance = $env:DB_INSTANCE,
-    [string]$Database = $env:TARGET_DB,
+    [string]$Database = $env:DATABASE,
     [bool]$IsAzureSQL = [System.Convert]::ToBoolean($env:AzureSQL),
     [string]$User = $env:AZURE_SQL_USER,
     [string]$Pass = $env:AZURE_SQL_PASS,
@@ -17,14 +17,13 @@ param(
     [switch]$CodeCoverage
 )
 
-. ".\tests\constants.ps1"
 $ErrorActionPreference = "Stop"
 $TestFiles = Get-ChildItem -Path .\tests\*.Tests.ps1
 $FailedTests = 0
 
 # Run Tests
 ForEach ($file in $TestFiles) {
-    If (!$LocalTest.IsPresent) { Add-AppveyorTest -Name $file.BaseName -Framework NUnit -Filename $file.FullName -Outcome Running }
+    Add-AppveyorTest -Name $file.BaseName -Framework NUnit -Filename $file.FullName -Outcome Running
 
     $PesterResult = Invoke-Pester -Path $file.FullName -Output Detailed -PassThru
     $Outcome = "Passed"
@@ -33,7 +32,7 @@ ForEach ($file in $TestFiles) {
         $FailedTests ++
     }
 
-    If (!$LocalTest.IsPresent) { Update-AppveyorTest -Name $file.BaseName -Framework NUnit -FileName $file.FullName -Outcome $Outcome -Duration $PesterResult.UserDuration.Milliseconds }
+    Update-AppveyorTest -Name $file.BaseName -Framework NUnit -FileName $file.FullName -Outcome $Outcome -Duration $PesterResult.UserDuration.Milliseconds
 }
 
 # Check for failures
