9492f489ad9d09636a90cd99bb5b8b3ff33197eb
John McCall <john@lowlydba.com>
2022-05-30 14:58:38 -0400

    fix output name

--- .github/actions/mssql-sample-db/main.ps1
+++ .github/actions/mssql-sample-db/main.ps1
@@ -10,15 +10,19 @@ if ($Env:RUNNER_OS -ne "Windows") {
     Write-Error "This action only supported on Windows runners." -ErrorAction "Stop"
 }
 
+Write-Output "Thanks for using $Env:GITHUB_ACTION!"
+
 if ($Database -eq "WideWorldImporters") {
     $BackupPath = Join-Path -Path $Env:RUNNER_TEMP -ChildPath "$Database.bak"
-    Write-Output "Downloading '$Database' to $BackupPath ..."
     $Uri = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
-    Invoke-WebRequest -Uri $Uri -OutFile $BackupPath
-    Write-Output "Restoring '$Database' database ..."
-    $null = Restore-DbaDatabase -SqlInstance $SqlInstance -DatabaseName $Database -Path $BackupPath -WithReplace -EnableException
-    Get-DbaDatabase -SqlInstance $SqlInstance -Database $Database
+    $Documentation = "https://docs.microsoft.com/en-us/sql/samples/wide-world-importers-what-is"
 }
-else {
-    Write-Output "$Database not supported yet - try opening a PR!"
-}
+
+Write-Output "Documentation: $Documentation"
+Write-Output ""
+
+Write-Output "Downloading '$Database' to $BackupPath ..."
+Invoke-WebRequest -Uri $Uri -OutFile $BackupPath
+Write-Output "Restoring '$Database' database ..."
+$null = Restore-DbaDatabase -SqlInstance $SqlInstance -DatabaseName $Database -Path $BackupPath -WithReplace -EnableException
+Get-DbaDatabase -SqlInstance $SqlInstance -Database $Database | Select-Object -Property Name, Status, SizeMB, SqlInstance
