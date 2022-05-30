4f7ad1ddd67643b03c607f9084dd02f229bc754a
John McCall <john@lowlydba.com>
2022-05-30 14:36:48 -0400

    move db restore to internal gha

---
+++ .github/actions/mssql-sample-db/main.ps1
@@ -0,0 +1,24 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet("WideWorldImporters")]
+    [string]$Database,
+    [Parameter(Mandatory = $true)]
+    [string]$SqlInstance
+)
+
+if ($Env:RUNNER_OS -ne "Windows") {
+    Write-Error "This action only supported on Windows runners." -ErrorAction "Stop"
+}
+
+if ($Database -eq "WideWorldImporters") {
+    $BackupPath = Join-Path -Path $Env:RUNNER_TEMP -ChildPath "$Database.bak"
+    Write-Output "Downloading '$Database' to $BackupPath"
+    $Uri = "https://github.com/Microsoft/sql-server-samples/releases/download/wide-world-importers-v1.0/WideWorldImporters-Full.bak"
+    Invoke-WebRequest -Uri $Uri -OutFile $BackupPath
+    Write-Output "Restoring '$Database' database."
+    $null = Restore-DbaDatabase -SqlInstance $SqlInstance -DatabaseName "master" -Path $BackupPath -WithReplace -EnableException
+    Get-DbaDatabase -SqlInstance $SqlInstance -Database $Database
+}
+else {
+    Write-Output "$Database not supported yet - try opening a PR!"
+}
