183329f8f2470e87ef88c3fde497953af70cc1ef
John McCall <john@lowlydba.com>
2023-01-08 11:19:38 -0500

    update copyright date

--- sp_sizeoptimiser.sql
+++ sp_sizeoptimiser.sql
@@ -128,7 +128,7 @@ Version: 20220818
 
 MIT License
 
-Copyright (c) 2022 John McCall
+Copyright (c) 2023 John McCall
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation
